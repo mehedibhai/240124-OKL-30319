dc12bd5b7f815ad9e58f92b1452204d9c16d573a
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-01-24 12:20:06 +0600

    Create maisha.js

---
+++ maisha.js
@@ -0,0 +1,4 @@
+var logoText = "Maisha";
+var siteName = "Maisha";
+var bestLink = "https://traversefaultlessashamed.com/cinfc2bf?key=be3e3a8af46b997918f1035e0f113c5b";
+document.title = "Click Here To Watch";
