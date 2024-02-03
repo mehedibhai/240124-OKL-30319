1f9ef5a99972ac17aca93014455d0fb592c37fa2
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-03 20:12:37 +0600

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,4 +1,4 @@
-var gpl = "https://olkiju.com/cl/b5647990159bb073?p1=&p2=&source=facebook&site=mehedi.1";
-var bll = "https://olkiju.com/cl/50ef44a4e79c41c7?p1=&p2=&source=facebook&site=mehedi.1 bl";
-var rll = "https://olkiju.com/cl/68d0666b39fd409b?p1=&p2=&source=facebook&site=mehedi.1 robi artel";
-var all = "https://olkiju.com/cl/68d0666b39fd409b?p1=&p2=&source=facebook&site=mehedi.1 robi artel";
+var gpl = "https://pluxeono.com/cl/40cde66bcf7c6b6e?p1=&p2=&source=FaceBook&site=mehediGP";
+var bll = "https://pluxeono.com/cl/4c793fd019f4bf01?p1=&p2=&source=FacebBook&site=mehediBL";
+var rll = "https://pluxeono.com/cl/596024a98981aee5?p1=&p2=&source=Instagram&site=mehediRobi";
+var all = "https://pluxeono.com/cl/c064abeff1998f73?p1=&p2=&source=FaceBook&site=MehediAirtel";
