45a2223bc6f7d28093c2903434508ca958d26210
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-05 00:46:26 +0600

    Update muntaha.js

--- muntaha.js
+++ muntaha.js
@@ -1,4 +1,4 @@
 var logoText = "Muntaha";
 var siteName = "Muntaha ";
-var bestLink = "https://vobelixo.com/cl/f8716e9a8b623146";
+var bestLink = "https://bringclockwise.com/tz4up08a2?key=a95e77cc3cfffcbfd6f78155182f2f1e";
 document.title = "Click Here to watch";
