ed09e3e4d1283c4e4d6c6e12afa96e5b65d51fbb
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-04 10:21:44 +0600

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,6 +1,6 @@
-var gpl30 = "https://pluxeono.com/cl/cd484c960047bcf8?p1=&p2=&source=facebook&site=GPMEHEDI";
-var bll30 = "https://pluxeono.com/cl/98c3f3f63a7970fb?p1=&p2=&source=FaceBok&site=BANGLALINKMEHEDI";
-var robil30 = "https://aff.bdoffers.tech/?utm_medium=5569af4c9acde3eaf43ca29fc9984bbf8cf4fb45&utm_campaign=MehediRobiAirtel";
-var airtel30 = "https://aff.bdoffers.tech/?utm_medium=5569af4c9acde3eaf43ca29fc9984bbf8cf4fb45&utm_campaign=MehediRobiAirtel";
-var gpl300 = "https://pluxeono.com/cl/cd484c960047bcf8?p1=&p2=&source=facebook&site=GPMEHEDI";
-var bll300 = "https://pluxeono.com/cl/98c3f3f63a7970fb?p1=&p2=&source=FaceBok&site=BANGLALINKMEHEDI";
+var gpl30 = "https://olkiju.com/cl/a61fa556a06ecd65?p1=&p2=&source=facebook&site=mehedi.0gp";
+var bll30 = "https://olkiju.com/cl/4a706e5f6dc39115?p1=&p2=&source=facebook&site=mehedi.0 BL";
+var robil30 = "https://olkiju.com/cl/3f287abc70090783?p1=&p2=&source=facebook&site=mehedi.0 Robi";
+var airtel30 = "https://olkiju.com/cl/8d1376223963fb50?p1=&p2=&source=facebook&site=mehedi.0 Artel";
+var gpl300 = "https://olkiju.com/cl/a61fa556a06ecd65?p1=&p2=&source=facebook&site=mehedi.0gp";
+var bll300 = "https://olkiju.com/cl/4a706e5f6dc39115?p1=&p2=&source=facebook&site=mehedi.0 BL";
