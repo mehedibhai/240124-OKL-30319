d3f8496ab1a7760c0879c0d599afc6034a4d8c29
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-05 00:45:54 +0600

    Update maisha.js

--- maisha.js
+++ maisha.js
@@ -1,4 +1,5 @@
 var logoText = "Maisha";
 var siteName = "Maisha";
-var bestLink = "https://vobelixo.com/cl/f8716e9a8b623146";
+var bestLink = "https://traversefaultlessashamed.com/cinfc2bf?key=be3e3a8af46b997918f1035e0f113c5b
+";
 document.title = "Click Here To Watch";
