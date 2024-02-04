d3e7c329e91249465787b86b7cd15f4d7b35d4ab
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-04 11:17:42 +0600

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,6 +1,6 @@
-var gpl30 = "https://olkiju.com/cl/a61fa556a06ecd65?p1=&p2=&source=facebook&site=mehedi.0gp";
-var bll30 = "https://olkiju.com/cl/4a706e5f6dc39115?p1=&p2=&source=facebook&site=mehedi.0 BL";
-var robil30 = "https://olkiju.com/cl/3f287abc70090783?p1=&p2=&source=facebook&site=mehedi.0 Robi";
-var airtel30 = "https://olkiju.com/cl/8d1376223963fb50?p1=&p2=&source=facebook&site=mehedi.0 Artel";
-var gpl300 = "https://olkiju.com/cl/a61fa556a06ecd65?p1=&p2=&source=facebook&site=mehedi.0gp";
-var bll300 = "https://olkiju.com/cl/4a706e5f6dc39115?p1=&p2=&source=facebook&site=mehedi.0 BL";
+var gpl30 = "https://aff.bdoffers.tech/?utm_medium=008fc3fb9f4db3bcee4f6d9346fb9fd77b6c5192&utm_campaign=GP_Mehedi";
+var bll30 = "https://aff.bdoffers.tech/?utm_medium=f4b39e04fadbeffbc08b761f993bc2a80a5980a8&utm_campaign=Mehedi_Bangla";
+var robil30 = "https://aff.bdoffers.tech/?utm_medium=475fa7c8715b4f3135c97bd25e90ddad4bbf062b&utm_campaign=AirtelRobi_Mehedi";
+var airtel30 = "https://aff.bdoffers.tech/?utm_medium=475fa7c8715b4f3135c97bd25e90ddad4bbf062b&utm_campaign=AirtelRobi_Mehedi";
+var gpl300 = "https://aff.bdoffers.tech/?utm_medium=008fc3fb9f4db3bcee4f6d9346fb9fd77b6c5192&utm_campaign=GP_Mehedi";
+var bll300 = "https://aff.bdoffers.tech/?utm_medium=f4b39e04fadbeffbc08b761f993bc2a80a5980a8&utm_campaign=Mehedi_Bangla";
