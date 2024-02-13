6660867b91b5dc9f8bca2342ad666fa6d121c3ed
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-13 11:43:52 +0600

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,5 +1,5 @@
-var gpl30 = "https://aff.bdoffers.tech/?utm_medium=bcb18495bac4aa6c423b12c0efe57bf06ae42885&utm_campaign=mehedigpx";
-var bll30 = "https://aff.bdoffers.tech/?utm_medium=ff131f586807a234b77b1e356b0675fcd172ddc6&utm_campaign=mehediblx";
+var gpl30 = "https://snozxara.com/cl/1cd6e615d34718b8?p1=&p2=&source=facebook&site=momin";
+var bll30 = "https://snozxara.com/cl/1cd6e615d34718b8?p1=&p2=&source=facebook&site=momin";
 var robil30 = "https://aff.bdoffers.tech/?utm_medium=69a10fd140bd5f02bd961e28039909675b56e2ab&utm_campaign=mehediarxx";
 var airtel30 = "https://aff.bdoffers.tech/?utm_medium=69a10fd140bd5f02bd961e28039909675b56e2ab&utm_campaign=mehediarxx";
 var gpl300 = "https://aff.bdoffers.tech/?utm_medium=bcb18495bac4aa6c423b12c0efe57bf06ae42885&utm_campaign=mehedigpx";
