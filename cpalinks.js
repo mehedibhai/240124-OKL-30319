adcd90e9aa37509926c99f1d0d98ce801120fd9f
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-19 22:02:05 -0800

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=aa79fb6a031255fe74313d694807afe786260adb&utm_campaign=mehedigp44";
+var gplink = "https://abdul.b-d.pw/?utm_medium=39a9da08ecd76dbfc3bed514ede5e0c785ebdd31&utm_campaign=mehedigp22";
 var bllink = "https://abdul.b-d.pw/?utm_medium=aa52a0c4136788a97e74c2e20fa14778cf9c5d9f&utm_campaign=mehedi4";
 var arlink = "https://abdul.b-d.pw/?utm_medium=ae8f254ba955cad6f743c329639842f3b832e4af&utm_campaign=MEHEDI22";
 var rblink = "https://abdul.b-d.pw/?utm_medium=ae8f254ba955cad6f743c329639842f3b832e4af&utm_campaign=MEHEDI22";
