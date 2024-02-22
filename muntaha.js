c2f24ce2dcdc27acc6caa1f55f12ac86f3df7bd0
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-22 14:09:38 +0600

    Delete muntaha.js

--- muntaha.js
+++
@@ -1,4 +0,0 @@
-var logoText = "Muntaha";
-var siteName = "Muntaha ";
-var bestLink = "https://bringclockwise.com/tz4up08a2?key=a95e77cc3cfffcbfd6f78155182f2f1e";
-document.title = "Click Here to watch";
