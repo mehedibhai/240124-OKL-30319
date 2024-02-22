e6744746215d769049b213a62c4637e5cca851b7
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-22 14:09:24 +0600

    Delete maisha.js

--- maisha.js
+++
@@ -1,5 +0,0 @@
-var logoText = "Maisha";
-var siteName = "Maisha";
-var bestLink = "https://traversefaultlessashamed.com/cinfc2bf?key=be3e3a8af46b997918f1035e0f113c5b
-";
-document.title = "Click Here To Watch";
