a62d42f0455489d7d725f3add70b7bebacb86390
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-24 04:08:19 -0800

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
 var gplink = "https://fynddyx.com/cl/63eee4bee81c07ac?p1=&p2=&source=facebook&site=yousufgp32";
-var bllink = "https://plivexx.com/cl/d70f63be03d76e5e?p1=&p2=&source=facebook&site=yousufbl24";
+var bllink = "https://flibarox.com/cl/cd28af315f1fa27e?p1=&p2=&source=facebook&site=yousufbl28";
 var arlink = "https://plivexx.com/cl/59d53a0fe8b68adb?p1=&p2=&source=facebook&site=yousuf32airtel";
 var rblink = "https://plivexx.com/cl/59d53a0fe8b68adb?p1=&p2=&source=facebook&site=yousuf32airtel";
