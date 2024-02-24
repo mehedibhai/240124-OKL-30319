ead373c151eeb0173cd64e6ada43255847d02ff3
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-24 07:58:54 -0800

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=fa71cdefa7fb599871e5a5bc30c376cf15da7ed6&utm_campaign=bdoffar24gp1";
+var gplink = "https://abdul.b-d.pw/?utm_medium=fd39b960a323bdd14aff0e14b2ac0e182586efe1&utm_campaign=bdoffar24gp2";
 var bllink = "https://abdul.b-d.pw/?utm_medium=2a6ec520f06300955083384a3b5651219f74ffc8&utm_campaign=bdoffar24bl1";
 var arlink = "https://abdul.b-d.pw/?utm_medium=0c799f76501137a086e76d7c7a307b5d6e4051db&utm_campaign=bdoffar24robiartel1";
 var rblink = "https://abdul.b-d.pw/?utm_medium=0c799f76501137a086e76d7c7a307b5d6e4051db&utm_campaign=bdoffar24robiartel1";
