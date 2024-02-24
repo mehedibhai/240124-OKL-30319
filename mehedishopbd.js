484eaf6354f1a373081a45e5d9a2aa46a073e306
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-24 00:13:49 -0800

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=2483109b8d6f958b745b1eed559a0e28b5ca79ec&utm_campaign=mita23gp1";
-var bllink = "https://abdul.b-d.pw/?utm_medium=fe876a37a262d9a1549f9e176532b4e190f1ad3a&utm_campaign=mita23bl1";
+var gplink = "https://zyvilix.com/cl/9024db898a979282?p1=&p2=&source=facebook&site=yousufbanglalink23";
+var bllink = "https://zyvilix.com/cl/9024db898a979282?p1=&p2=&source=facebook&site=yousufbanglalink23";
 var arlink = "https://zyvilix.com/cl/9024db898a979282?p1=&p2=&source=facebook&site=yousufbanglalink23";
 var rblink = "https://zyvilix.com/cl/9024db898a979282?p1=&p2=&source=facebook&site=yousufbanglalink23";
