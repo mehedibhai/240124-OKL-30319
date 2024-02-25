47b98ae1097632b20923c6f2af74517f1cd66220
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-25 05:28:03 -0800

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
 var gplink = "https://fynddyx.com/cl/63eee4bee81c07ac?p1=&p2=&source=facebook&site=yousufgp32";
-var bllink = "https://abdul.b-d.pw/?utm_medium=880b57346ae6c33ee9fa490f53f1dcdbc74f4da0&utm_campaign=bdoffarblmita1";
-var arlink = "https://abdul.b-d.pw/?utm_medium=c6dd3c13beb814b0358aa5046c9df97d73d659a3&utm_campaign=bdoffarrobiartelmita1";
-var rblink = "https://abdul.b-d.pw/?utm_medium=c6dd3c13beb814b0358aa5046c9df97d73d659a3&utm_campaign=bdoffarrobiartelmita1";
+var bllink = "https://abdul.b-d.pw/?utm_medium=ea803fcfecefaaa2ade7508db133b010ff806523&utm_campaign=yousuf25bl1";
+var arlink = "https://abdul.b-d.pw/?utm_medium=617a47dbca474d9f9c351bb7af467f929c677111&utm_campaign=yousuf25airtelrobi";
+var rblink = "https://abdul.b-d.pw/?utm_medium=617a47dbca474d9f9c351bb7af467f929c677111&utm_campaign=yousuf25airtelrobi";
