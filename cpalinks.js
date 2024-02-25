fd9355fe8359a961127b89f134435c26909e5400
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-25 06:50:04 -0800

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,4 +1,4 @@
 var gplink = "https://abdul.b-d.pw/?utm_medium=fd39b960a323bdd14aff0e14b2ac0e182586efe1&utm_campaign=bdoffar24gp2";
-var bllink = "https://abdul.b-d.pw/?utm_medium=5548b8941816a26baafc9f440676a32df4dcef74&utm_campaign=bdoffar25gp1";
+var bllink = "https://abdul.b-d.pw/?utm_medium=2a6ec520f06300955083384a3b5651219f74ffc8&utm_campaign=bdoffar24bl1";
 var arlink = "https://abdul.b-d.pw/?utm_medium=b7d452243f52815bb0e1665e9703daf434fa7a6f&utm_campaign=offerrobiairtel25";
 var rblink = "https://abdul.b-d.pw/?utm_medium=b7d452243f52815bb0e1665e9703daf434fa7a6f&utm_campaign=offerrobiairtel25";
