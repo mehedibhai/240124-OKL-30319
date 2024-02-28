6e25799d9b1fcbfdff06e6f9f591254f3d8a49e4
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-02-28 06:26:37 -0800

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,4 +1,4 @@
 var gplink = "https://plivexx.com/cl/aa671adc3b75bccf?p1=&p2=&source=facebook&site=mim.1";
 var bllink = "https://plivexx.com/cl/03df8f713cc43a16?p1=&p2=&source=facebook&site=mimbl.1";
 var arlink = "https://plivexx.com/cl/9f914b88e27a8560?p1=&p2=&source=facebook&site=mimar.1";
-var rblink = "https://plivexx.com/cl/24a0f819e8011b37?p1=&p2=&source=facebook&site=mim.1";
+var rblink = "https://plivexx.com/cl/9502798b9495092d?p1=&p2=&source=facebook&site=mimrobi.1";
