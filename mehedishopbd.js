90e5750d322174d440c5d444f4b03d2ef0c1e247
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-02 01:24:33 -0800

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibblix.com/cl/a2d9579b63a243ae?p1=&p2=&source=facebook&site=yousuf28gp";
-var bllink = "https://flibblix.com/cl/b877377ef7d8bfe2?p1=&p2=&source=facebook&site=yousuf28bl";
+var gplink = "https://zyvilix.com/cl/86401bd2caa023ba?p1=&p2=&source=facebook&site=yousuf20gpbl";
+var bllink = "https://zyvilix.com/cl/86401bd2caa023ba?p1=&p2=&source=facebook&site=yousuf20gpbl";
 var arlink = "https://flibblix.com/cl/d1d6164c6f0840a4?p1=&p2=&source=facebook&site=yousuf28airtel";
-var rblink = "https://flibblix.com/cl/5b95a24aeea79ee7?p1=&p2=&source=facebook&site=yousuf27robi";
+var rblink = "https://flibblix.com/cl/d1d6164c6f0840a4?p1=&p2=&source=facebook&site=yousuf28airtel";
