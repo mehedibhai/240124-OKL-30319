1ae6519fa3bea2b57509c1041a57457b9af10c53
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-11 13:59:33 +0000

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=85a109a6e0f7311a24588b5eb578ade585c601d0&utm_campaign=mehedigp1";
-var bllink = "https://abdul.b-d.pw/?utm_medium=e41027afc6d8ccc4528150ff5e8c4efe66450fe5&utm_campaign=mehedibl12";
+var gplink = "https://abdul.b-d.pw/?utm_medium=be1663be555b834cb558623631f1887f70185ced&utm_campaign=mehedigp21";
+var bllink = "https://abdul.b-d.pw/?utm_medium=d0774580387039bddb5e0d0a08c6184a174fe47b&utm_campaign=mehedibl21";
 var arlink = "https://abdul.b-d.pw/?utm_medium=b593aa1ce45c5342e43905042b9ac4fa5369d5b7&utm_campaign=mehedirobiartel1";
 var rblink = "https://abdul.b-d.pw/?utm_medium=b593aa1ce45c5342e43905042b9ac4fa5369d5b7&utm_campaign=mehedirobiartel1";
