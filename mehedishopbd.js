031532c174bda7bcf54b4b8be444644bae3c2d40
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-12 10:14:33 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=b1eef1460c60447520f3cb874dc9f2aee625a5ae&utm_campaign=mehedigp0";
-var bllink = "https://abdul.b-d.pw/?utm_medium=c3f4becb5e80e5badc4cbd6e1fcaf8dc072935ae&utm_campaign=mehedi07bal";
+var gplink = "https://fynddyx.com/cl/da57beecb6c57d70?p1=&p2=&source=facebook&site=mahedigp007";
+var bllink = "https://fynddyx.com/cl/312190d3909cb849?p1=&p2=&source=facebook&site=mahedibl007";
 var arlink = "https://abdul.b-d.pw/?utm_medium=4d8abb253354962d304d1b201c26791db7ce4086&utm_campaign=mehedi07ar";
-var rblink = "https://abdul.b-d.pw/?utm_medium=4d8abb253354962d304d1b201c26791db7ce4086&utm_campaign=mehedi07ar";
+var rblink = "https://fynddyx.com/cl/218e30fe60294c11?p1=&p2=&source=facebook&site=mahedirobi007";
