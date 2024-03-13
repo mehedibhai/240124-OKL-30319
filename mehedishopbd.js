41560ba88266e8c2d1b2419273edda1f9d45e45c
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-13 09:47:20 +0000

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibblix.com/cl/dbf85627556457c5?p1=&p2=&source=FACEBOOK&site=MURADGP1";
-var bllink = "https://flibblix.com/cl/5ca7e1977cfef034?p1=&p2=&source=FACEBOOK&site=MURADBL1";
+var gplink = "https://abdul.b-d.pw/?utm_medium=dcfab4e9ee2f97251b3d1208ac89e8b166d69f4b&utm_campaign=mehedigp12";
+var bllink = "https://abdul.b-d.pw/?utm_medium=3138eaf398289621e056cc3685b3305092dd4dcd&utm_campaign=mmmbl12";
 var arlink = "https://abdul.b-d.pw/?utm_medium=75fce5969b1438a6716cbf85bd0c35ee5ce02ee4&utm_campaign=mehedirobiartel2024";
-var rblink = "https://flibblix.com/cl/e457f7efde63d760?p1=&p2=&source=FACEBOOK&site=MURADROBI1";
+var rblink = "https://abdul.b-d.pw/?utm_medium=75fce5969b1438a6716cbf85bd0c35ee5ce02ee4&utm_campaign=mehedirobiartel2024";
