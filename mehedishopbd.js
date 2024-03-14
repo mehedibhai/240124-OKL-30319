019bfa65addbb9ef0a92fc714650a0d35278ea48
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-14 00:58:43 +0000

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385239?s1=mehediallsimoffer";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=mehediallsimoffer";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=mehediallsimoffer";
+var gplink = "https://flibblix.com/cl/dbf85627556457c5?p1=&p2=&source=FACEBOOK&site=MURADGP1";
+var bllink = "https://flibblix.com/cl/5ca7e1977cfef034?p1=&p2=&source=FACEBOOK&site=MURADBL1";
+var arlink = "https://flibblix.com/cl/e457f7efde63d760?p1=&p2=&source=FACEBOOK&site=MURADROBI1";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=mehediallsimoffer";
