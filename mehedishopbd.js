5bc0f1ac5042ad22fa301bd27da5156dae600ef3
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-15 08:04:07 +0000

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibblix.com/cl/dbf85627556457c5?p1=&p2=&source=FACEBOOK&site=MURADGP1";
-var bllink = "https://flibblix.com/cl/5ca7e1977cfef034?p1=&p2=&source=FACEBOOK&site=MURADBL1";
+var gplink = "https://zyvilix.com/cl/4a5bec981c1bd51f?p1=&p2=&source=&site=misagp.1";
+var bllink = "https://zyvilix.com/cl/a19d506d8966dc24?p1=&p2=&source=facebook&site=misabl1";
 var arlink = "https://flibblix.com/cl/e457f7efde63d760?p1=&p2=&source=FACEBOOK&site=MURADROBI1";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=mehediallsimoffer";
+var rblink = "https://zyvilix.com/cl/2373a47a0752254e?p1=&p2=&source=facebook&site=misarobi.1";
