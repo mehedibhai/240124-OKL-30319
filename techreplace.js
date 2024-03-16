8cf37fcc5a6614e22eb337ea1882c9b8fbcdeab9
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-16 09:17:10 +0000

    Update techreplace.js

--- techreplace.js
+++ techreplace.js
@@ -1,4 +1,4 @@
 var gplink = "https://abdul.b-d.pw/?utm_medium=aeef1fa3cbf9517f3631b58f6f0081faca87be7e&utm_campaign=mygp2";
-var bllink = "https://abdul.b-d.pw/?utm_medium=1f18dfa8b0d78d8dbbdadfba48e5967d45920122&utm_campaign=BANGLALINK1";
+var bllink = "https://abdul.b-d.pw/?utm_medium=db6e9d0acc3b38352e8f342b276cf3bc201e8718&utm_campaign=BANGLALINK2";
 var arlink = "https://abdul.b-d.pw/?utm_medium=b0b2fce573d5641f7db0e7b0bf76fa17c149b1ae&utm_campaign=AIRTELBDROBI";
 var rblink = "https://abdul.b-d.pw/?utm_medium=b0b2fce573d5641f7db0e7b0bf76fa17c149b1ae&utm_campaign=AIRTELBDROBI";
