f1fac6271fd0b9dfa7bd5c3032d026d950e41a7a
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-03-21 08:16:47 +0000

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=316c8c6269eb87a907ab13dce20ec9582a149775&utm_campaign=ROMADANGP1";
+var gplink = "https://hpmyrik.com/cl/b62c641586ef96eb?p1=&p2=&source=facebook&site=romadangp1";
 var bllink = "https://abdul.b-d.pw/?utm_medium=c757d77879924ce1cd0c38a366664738f9a386e0&utm_campaign=ROMADANBL1";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=mehediallxx";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=mehediallxx";
+var arlink = "https://hpmyrik.com/cl/e3a13761b06f3674?p1=&p2=&source=facebook&site=romadanrobi1";
+var rblink = "https://hpmyrik.com/cl/e3a13761b06f3674?p1=&p2=&source=facebook&site=romadanrobi1";
