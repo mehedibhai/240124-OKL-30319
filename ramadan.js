04e2c40df2bc0a33dd2d75dc8d4b0b3d49faba03
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-04-03 06:19:00 +0600

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=e49615f6540a0c95549cbfdc310bd509b392686b&utm_campaign=RIYADGP1";
+var gplink = "https://abdul.b-d.pw/?utm_medium=90d79335b46cf612e34d5c31ae55001f40e2b492&utm_campaign=Riyadgp2";
 var bllink = "https://abdul.b-d.pw/?utm_medium=40e6509b07892d1f36ec59ce51ff8fb69e54f74a&utm_campaign=RIYADBL1";
-var arlink = "https://abdul.b-d.pw/?utm_medium=393bc236712cf68cb9e9f96bd23caae0c30fdcb5&utm_campaign=RIYADARTELROBI1";
-var rblink = "https://abdul.b-d.pw/?utm_medium=393bc236712cf68cb9e9f96bd23caae0c30fdcb5&utm_campaign=RIYADARTELROBI1";
+var arlink = "https://abdul.b-d.pw/?utm_medium=95184c71c851a4a1143e9c44dd4592be1313d7b0&utm_campaign=Ridarrobi2";
+var rblink = "https://abdul.b-d.pw/?utm_medium=95184c71c851a4a1143e9c44dd4592be1313d7b0&utm_campaign=Ridarrobi2";
