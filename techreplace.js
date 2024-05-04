c01ddcada075aed576657f2fa6bbe98aeedddabd
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-04 08:49:07 +0100

    Update techreplace.js

--- techreplace.js
+++ techreplace.js
@@ -1,4 +1,4 @@
 var gplink = "https://abdul.b-d.pw/?utm_medium=f3adfd59dae3cb41bebae92131ed7a9ac8e4679c&utm_campaign=mehedigp24";
 var bllink = "https://abdul.b-d.pw/?utm_medium=6c69d25fcc28a915af7a51e821d81b12308ff770&utm_campaign=BL04";
-var arlink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
-var rblink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
+var arlink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
+var rblink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
