2523ec0e8dbeb6224965e2e561fe6fea4a3b87be
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-04 13:20:08 +0100

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=f3adfd59dae3cb41bebae92131ed7a9ac8e4679c&utm_campaign=mehedigp24";
+var gplink = "https://abdul.b-d.pw/?utm_medium=3e06cba4723b9a624a821aaab2284e7c78682e2d&utm_campaign=GP04";
 var bllink = "https://abdul.b-d.pw/?utm_medium=dbbe87993be611046f8c20ad3a577a6e5dd2faae&utm_campaign=target_BD_d4b168";
-var arlink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
-var rblink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
+var arlink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
+var rblink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
