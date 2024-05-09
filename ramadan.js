baffcda11f201920ac35f981a023cfb5f9915869
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-09 10:13:07 +0100

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=f3adfd59dae3cb41bebae92131ed7a9ac8e4679c&utm_campaign=mehedigp24";
-var bllink = "https://abdul.b-d.pw/?utm_medium=d99db283c6b20b353637047c03030d3d3f7d320e&utm_campaign=bl";
+var gplink = "https://abdul.b-d.pw/?utm_medium=d9a6be9a1fb22077e41bebd0eced4733c90f984e&utm_campaign=gp";
+var bllink = "https://abdul.b-d.pw/?utm_medium=1f6e461981984abe2979a4a2afd72f47714a612f&utm_campaign=Bl111";
 var arlink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
 var rblink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
