10857d8e1f8129e7fdce0d6f64cda3f7bd50a848
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-10 05:38:07 +0100

    Update techreplace.js

--- techreplace.js
+++ techreplace.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=3e06cba4723b9a624a821aaab2284e7c78682e2d&utm_campaign=GP04";
-var bllink = "https://abdul.b-d.pw/?utm_medium=6c69d25fcc28a915af7a51e821d81b12308ff770&utm_campaign=BL04";
+var gplink = "https://zypholuxa.com/cl/fb3be145b5e05a3f?p1=&p2=&source=tiktok&site=GP";
+var bllink = "https://zypholuxa.com/cl/365de2f08afe930a?p1=&p2=&source=&site=tiktok";
 var arlink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
-var rblink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
+var rblink = "https://zypholuxa.com/cl/bfc65e8682595452?p1=&p2=&source=&site=tiktok";
