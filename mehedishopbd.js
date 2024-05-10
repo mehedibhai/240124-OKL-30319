62217662e7c3c7fe7667487839a2c69f1ea99856
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-10 05:36:46 +0100

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://abdul.b-d.pw/?utm_medium=d9a6be9a1fb22077e41bebd0eced4733c90f984e&utm_campaign=gp";
-var bllink = "https://abdul.b-d.pw/?utm_medium=1f6e461981984abe2979a4a2afd72f47714a612f&utm_campaign=Bl111";
+var gplink = "https://zypholuxa.com/cl/fb3be145b5e05a3f?p1=&p2=&source=tiktok&site=GP";
+var bllink = "https://zypholuxa.com/cl/365de2f08afe930a?p1=&p2=&source=&site=tiktok";
 var arlink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
-var rblink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
+var rblink = "https://zypholuxa.com/cl/bfc65e8682595452?p1=&p2=&source=&site=tiktok";
