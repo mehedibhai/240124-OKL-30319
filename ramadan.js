25fbb237d3ff980b1e5df6e4da80ed86025b842d
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-10 06:06:33 +0100

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://zypholuxa.com/cl/fb3be145b5e05a3f?p1=&p2=&source=tiktok&site=GP";
+var gplink = "https://zypholuxa.com/cl/894477d2c0023640?p1=&p2=&source=TIKTOK&site=GP";
 var bllink = "https://zypholuxa.com/cl/365de2f08afe930a?p1=&p2=&source=&site=tiktok";
 var arlink = "https://abdul.b-d.pw/?utm_medium=1b3ed27e5356a43a9d68fcb7e9fea3ca658ceece&utm_campaign=mehedirobi24";
 var rblink = "https://zypholuxa.com/cl/bfc65e8682595452?p1=&p2=&source=&site=tiktok";
