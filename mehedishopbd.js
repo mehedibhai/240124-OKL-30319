c6c8000b819f213e2bd6006809521e38d42b4e04
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-10 06:06:06 +0100

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://zypholuxa.com/cl/fb3be145b5e05a3f?p1=&p2=&source=tiktok&site=GP";
+var gplink = "https://zypholuxa.com/cl/894477d2c0023640?p1=&p2=&source=TIKTOK&site=GP";
 var bllink = "https://zypholuxa.com/cl/365de2f08afe930a?p1=&p2=&source=&site=tiktok";
 var arlink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
 var rblink = "https://zypholuxa.com/cl/bfc65e8682595452?p1=&p2=&source=&site=tiktok";
