6c9300880172e637a36f9fd8325399eafdf8d8fe
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-13 16:23:14 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://zypholuxa.com/cl/894477d2c0023640?p1=&p2=&source=TIKTOK&site=GP";
-var bllink = "https://nxtzlaf.com/cl/4b55f080ffb5d680?p1=&p2=&source=bl&site=nibir";
+var gplink = "https://abdul.b-d.pw/?utm_medium=241aa8ef9375df5918cd5fe799a95317a733bfcd&utm_campaign=Hoita11";
+var bllink = "https://abdul.b-d.pw/?utm_medium=5f6e6ce4ed5c491223692100618fc5186ab1f630&utm_campaign=Hoitabl11";
 var arlink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
-var rblink = "https://nxtzlaf.com/cl/4219816e7f1ebb32?p1=&p2=&source=robi&site=nibir";
+var rblink = "https://abdul.b-d.pw/?utm_medium=642a078e08a47e14686393febeaf2afe2c616788&utm_campaign=ROBI04";
